f434ee8af55001531b2722e259c05bc6dad2380e
Alvarogogal <agorsua0610@g.educaand.es>
2024-10-04 21:34:11 +0200

    Desde clase

--- Acceso_A_Datos/src/main/kotlin/XML/Actividad3/Main.kt
+++ Acceso_A_Datos/src/main/kotlin/XML/Actividad3/Main.kt
@@ -2,7 +2,12 @@ package XML.Actividad3
 
 import java.io.BufferedReader
 import java.io.FileReader
+import java.nio.file.Path
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 
 fun main(){
     val fr = FileReader("C:\\Users\\UsuarioT\\IdeaProjects\\AccesoADatos\\Acceso_A_Datos\\src\\main\\resources\\XML\\Actividad 3\\CSV.txt")
@@ -38,6 +43,35 @@ fun main(){
 
         document.documentElement.appendChild(empleado)
 
+        val id = document.createElement(tags[0])
+        val apellido = document.createElement(tags[1])
+        val departamento = document.createElement(tags[2])
+        val salario = document.createElement(tags[3])
+
+        val textId = document.createTextNode(empleados[i][0])
+        val textApel = document.createTextNode(empleados[i][1])
+        val textDep = document.createTextNode(empleados[i][2])
+        val textSal = document.createTextNode(empleados[i][3])
+
+        id.appendChild(textId)
+        apellido.appendChild(textApel)
+        departamento.appendChild(textDep)
+        salario.appendChild(textSal)
+
+        empleado.appendChild(id)
+        empleado.appendChild(apellido)
+        empleado.appendChild(departamento)
+        empleado.appendChild(salario)
 
     }
+
+    val source = DOMSource(document)
+
+    val r = StreamResult(Path.of("C:\\Users\\UsuarioT\\IdeaProjects\\AccesoADatos\\Acceso_A_Datos\\src\\main\\resources\\XML\\Actividad 3\\empleados.xml").toFile())
+
+    val optimus = TransformerFactory.newInstance().newTransformer()
+
+    optimus.setOutputProperty(OutputKeys.INDENT, "yes")
+
+    optimus.transform(source,r)
 }
